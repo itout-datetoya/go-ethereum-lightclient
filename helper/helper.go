b4003a07a1b19536c03dfde0ccca82390cacfcfc
itout-datetoya <b1122502@edu.kit.ac.jp>
2024-08-26 16:49:28 +0900

    helper: make helper functions public

--- helper/helper.go
+++ helper/helper.go
@@ -6,19 +6,19 @@ import (
 	"github.com/protolambda/ztyp/tree"
 )
 
-func computeSigningRoot(b types.BeaconBlockHeader, domain types.Domain) tree.Root {
+func ComputeSigningRoot(b types.BeaconBlockHeader, domain types.Domain) tree.Root {
 	hfn := tree.GetHashFn()
 	sData := types.SigningData{ObjectRoot: b.HashTreeRoot(hfn), Domain: domain}
 
 	return sData.HashTreeRoot(hfn)
 }
 
-func computeDomain(domainType types.DomainType, forkVersion types.Version, genesisValidatorsRoot tree.Root) types.Domain {
+func ComputeDomain(domainType types.DomainType, forkVersion types.Version, genesisValidatorsRoot tree.Root) types.Domain {
     forkDataRoot := computeForkDataRoot(forkVersion, genesisValidatorsRoot)
     return types.Domain(append(domainType, forkDataRoot[:28]...))
 }
 
-func computeForkDataRoot(currentVersion types.Version, genesisValidatorRoot tree.Root) tree.Root {
+func ComputeForkDataRoot(currentVersion types.Version, genesisValidatorRoot tree.Root) tree.Root {
 	hfn := tree.GetHashFn()
 	fData := types.ForkData{CurrentVersion: currentVersion, GenesisValidatorsRoot: genesisValidatorRoot}
 	return fData.HashTreeRoot(hfn)
