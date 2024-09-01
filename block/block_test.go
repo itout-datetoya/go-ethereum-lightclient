ea1e915244d570d8f83dd17cfb74aace5d8ebcb1
itout-datetoya <b1122502@edu.kit.ac.jp>
2024-09-01 18:50:48 +0900

    block: add block test

---
+++ block/block_test.go
@@ -0,0 +1,24 @@
+package block
+
+import (
+	"testing"
+	//"fmt"
+	"itout/go-ethereum-lightclient/util"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const EXE_URL_DEAULT = "https://mainnet.infura.io/v3/cdeb7402eca247e0a054717f350b4e50"
+const BEACON_URL_DEFAULT = "https://docs-demo.quiknode.pro/eth/v1/beacon/"
+
+const HASH = "0xacf662304a4dc3d37b112a5574c74343c8e6b30b0dfc7dcc16e4a494e53540ee"
+const NUMBER = 20654229
+const SLOT = 9862640
+const BEACON_HASH = "0x6838b423f31b3a8a8147a340fdc0b16345ca60b89d5963f54e15169e6b50f503"
+
+func TestBlock(t *testing.T) {
+	hash := util.HexstrTo32Bytes(HASH)
+	block := GetBlockByHash(hash, EXE_URL_DEAULT)
+
+	assert.Equal(t, NUMBER, int(block.number))
+}
